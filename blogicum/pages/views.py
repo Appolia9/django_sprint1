809c9fb5445842769ba5168462ca88b3202a58c8
Полина Минченко <minchenko2005@inbox.ru>
2026-01-08 21:55:57 +0300

    Conforming to PEP8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+
 def about(request):
-    template_name = 'pages/about.html'
+    template_name = "pages/about.html"
     context = {}
     return render(request, template_name, context)
 
+
 def rules(request):
-    template_name = 'pages/rules.html'
+    template_name = "pages/rules.html"
     context = {}
     return render(request, template_name, context)
